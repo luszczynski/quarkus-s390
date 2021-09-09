fe57469c349897662f1e362023aa2d45ec45fc3e
Gustavo Luszczynski <luszczynski@gmail.com>
2021-09-09 16:08:01 -0300

    initial commit

---
+++ src/main/java/com/redhat/ArchitectureResource.java
@@ -0,0 +1,30 @@
+package com.redhat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/arch")
+public class ArchitectureResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getArch() {
+        try {
+            Process p = Runtime.getRuntime().exec("uname -a");
+            String uname = new BufferedReader(new InputStreamReader(p.getInputStream())).readLine();
+            String[] array = uname.split(" ");
+            String arch = array[array.length-1];
+            return Response.status(200).entity(arch).build();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Response.status(500).entity(e.getMessage()).build();
+        }        
+    }
+}
---
+++ src/main/resources/META-INF/resources/dist/js/pages/arch.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Abdullah A Almsaeed
+ * Date: 4 Jan 2014
+ * Description:
+ *      This is a demo file used only for the main dashboard (index.html)
+ **/
+
+/* global moment:false, Chart:false, Sparkline:false */
+
+$(function () {
+    'use strict'
+
+    $.get( "arch", function( data ) {
+        if(data == "x86_64") {
+            $('#img-arch').html("<img src='imgs/aws.png' />");
+        }
+        else if(data == "s390") {
+            $('#img-arch').html("<img src='imgs/s390.png' />");
+        }
+        console.log("data = ", data);
+    });
+})
+  
